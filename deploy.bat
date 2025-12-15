189a8e875c8de9780f045ba046cd1434704ab735
JaniduGalappaththi <janidugalappaththi@gmail.com>
2025-12-15 13:58:14 +0530

    message

--- deploy.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo [Apps Script] Publishing to Live Server...
-call clasp push --force
-echo Done! Apps Script Updated.
--- git.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo [GitHub] Saving changes to GitHub only...
-git add .
-git commit -m "Manual Save: %date% %time%"
-git push
-echo Done! Saved to GitHub.
